d60f304aff872e406f7095564722cc1b2d53012a
Gabriel Sousa <104873711+GS-Gabriel@users.noreply.github.com>
2022-08-29 13:28:23 -0300

    Delete js directory

--- js/loading.js
+++
@@ -1,8 +0,0 @@
-const time = setTimeout(abrir, 50)
-    function abrir(){
-        document.querySelector('.img').src = "../img/anime_logomarca.gif"
-    }
-const time2 = setTimeout(abrir2, 4720)
-    function abrir2(){
-        window.location = '../pages/menu.html'
-    }
